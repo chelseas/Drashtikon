10dc3997edd190da8a6081fe3a3c97805ee8a3d4
Chelsea Sidrane <crs325@cornell.edu>
2016-11-13 20:44:03 -0800

    testing SVM

--- frame.py
+++ frame.py
@@ -1,4 +1,7 @@
 from frame_mod import *
+from SVM import *
+import numpy
+from skimage.feature import hog
 
 # a dummy function for now that labels everything 1
 def learning_method1(data, labels):
@@ -11,17 +14,23 @@ folder2 = 'folder2'
 
 # run learning methods
 # model should be a function that takes Image objects
-model = learning_method1(train_data,train_labels)
+print(size(numpy.array(train_data[0])))
+print(type(train_data[0]))
+#features = [numpy.ndarray.tolist(hog(numpy.ndarray(x))) for x in train_data]
+features = getHogFeatures(train_data)
+model = SVM(features,train_labels)
 
 # model should return an array of labels the same size as the test data
-predictions = model(test_data)
+predictions = model(getHogFeatures(test_data))
 print(predictions)
 
 # compute error
 sum_error = 0
 for i in range(len(test_data)):
-    if predictions[i]!=test_labels:
+    if predictions[i]!=test_labels[i]:
         sum_error+=1
 
 error = sum_error/len(test_data)
 print("Error is " + str(error))
+
+## TODO: print to textfile
