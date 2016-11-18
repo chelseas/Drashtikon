8d63d6420362e83197cc91feb652d401d75b7c10
Zach Maurer <zach.maurer@gmail.com>
2016-11-17 19:14:33 -0800

    Refactoring to accomodate more models.

--- frame.py
+++ frame.py
@@ -1,38 +1,56 @@
 from frame_mod import *
 from SVM import *
 import numpy
-from skimage.feature import hog
 import argparse
 
 parser = argparse.ArgumentParser(description='Framework that trains all learning options.')
-parser.add_argument('class1input' , action="store", help='Input Photo Directory, relative path to script directory')
-parser.add_argument('class2input' , action="store", help='Input Photo Directory, relative path to script directory')
+parser.add_argument('class1input' , action="store", help='Input Photo Directory, relative path')
+parser.add_argument('class2input' , action="store", help='Input Photo Directory, relative path')
+parser.add_argument('testSet' , action="store", help='Input Photo Directory, relative path')
+parser.add_argument('testClass' , action="store", help='Input integer for class 1 or class 2')
 args = parser.parse_args()
 
+MODELS = [rbfSVM, linearSVM]
+
+def calculateError(predictions, testLabels, msg=''):
+  sum_error = 0
+  for i in range(len(testLabels)):
+    if predictions[i] != testLabels[i]:
+        sum_error += 1
+  error = float(sum_error)/float(len(testLabels))
+  print(("\t {} Error is " + str(error)).format(msg)) 
+  return error
+
+
+def printInputStats(train1, train2, testData, testClass):
+  print("Class 1 Training Set: {} examples".format(train1))
+  print("Class 2 Training Set: {} examples".format(train2))
+  print("Test Set: Class {} with {} examples".format(testClass, train2))
 
 def main():
-  path = os.path.dirname(os.path.realpath(__file__))
+  path = os.getcwd()
   inputPath1 = os.path.join(path, args.class1input)
   inputPath2 = os.path.join(path, args.class2input)
-  (train_data,train_labels,test_data,test_labels) = import_data(inputPath1, inputPath2)
-  features = numpy.array([hog(x) for x in train_data])
-  model = SVM(features,train_labels)
-  predictions = model(numpy.array([hog(x) for x in test_data]))
-  
-  test_labels = numpy.array(test_labels)
-  
-  print(test_labels)
-  print(predictions)
+  testPath = os.path.join(path, args.testSet)
+  testClass= int(args.testClass)
+  #(train_data,train_labels,test_data,test_labels) = import_data(inputPath1, inputPath2)
+  (rawTrainData1, trainLabels1) = importData(inputPath1, 1)
+  (rawTrainData2, trainLabels2) = importData(inputPath2, 2)
+  (rawTestData, testLabels) = importData(testPath, testClass)
 
-  ## TODO: print to textfile
-  sum_error = 0
-  for i in range(len(test_labels)):
-    if predictions[i] != test_labels[i]:
-        sum_error+=1
-  error = float(sum_error)/float(len(test_data))
-  print("Error is " + str(error))
+  printInputStats(len(rawTrainData1), len(rawTrainData2), len(rawTestData), args.testClass)
 
+  trainFeatures = getHogFeatures(rawTrainData1 + rawTrainData2, "train data")
+  trainLabels = trainLabels1 + trainLabels2
+  testFeatures = getHogFeatures(rawTestData, "test data")
 
+  for m in MODELS:
+    model = m(trainFeatures, trainLabels)
+    testPredictions = model.predict(testFeatures)
+    trainPredictions = model.predict(trainFeatures)
+    testError = calculateError(testPredictions, testLabels, 'Test')
+    trainError = calculateError(trainPredictions, trainLabels, 'Train')
+    
 
 
 if __name__ == '__main__':
--- frame_mod.py
+++ frame_mod.py
@@ -6,12 +6,29 @@ from random import *
 from math import *
 import numpy as np
 import copy
+from skimage.feature import hog
 
-seed(0)
 p = re.compile('.*[.]jpg',re.IGNORECASE)
 
+def getHogFeatures(data, msg="data"):
+    print("Extracting HOG features for "+ msg + "...")
+    result = np.array([hog(x) for x in data])
+    print("Done.")
+    return result
+
+
+def importData(path, label):
+    data = []
+    for filename in os.listdir(path):
+        if p.match(filename):
+            img = np.array(Image.open(os.path.join(path, filename)))
+            data.append(img)
+    labels = [label for x in range(len(data))]
+    return (data, labels)
+
+
+
 def import_data(folder1, folder2):
-    # ##################
     # for each file in the folders, import the images and add to array
     loc1 = folder1
     list1 = []
@@ -20,7 +37,7 @@ def import_data(folder1, folder2):
             img = np.array(Image.open(os.path.join(loc1, filename)))
             list1.append(img)
     labels1 = [1 for x in range(len(list1))]
-    # ##################
+
     loc2 = folder2
     list2 = []
     for filename in os.listdir(loc2):
@@ -28,21 +45,9 @@ def import_data(folder1, folder2):
             img = np.array(Image.open(os.path.join(loc2, filename)))
             list2.append(img)
     labels2 = [2 for x in range(len(list2))]
-    # ##################
-    train_data = list1+list2
-    train_labels = labels1+labels2
-    # ##################
-    # TODO: randomly select examples to go into train and test
-    # test_data = []
-    # test_labels = []
-    # # can change. right now selecting 10% for testing
-    # for i in range(ceil(len(train_labels)/10)):
-    #     ind = randint(0,len(train_labels)-1)
-    #     test_labels.append(train_labels[ind])
-    #     test_data.append(train_data[ind])
-    #     del train_data[ind]
-    #     del train_labels[ind]
-    # ##################
-    test_data = copy.deepcopy(train_data)
-    test_labels = copy.deepcopy(train_labels)
-    return (train_data,train_labels,test_data,test_labels)
+
+    data = list1+list2
+    labels = labels1+labels2
+    # test_data = copy.deepcopy(train_data)
+    # test_labels = copy.deepcopy(train_labels)
+    return (data,labels)
