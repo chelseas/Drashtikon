4985e9ff8724c2859a9d1153a8b207a1968db64d
Chelsea Sidrane <crs325@cornell.edu>
2016-11-25 05:21:34 -0500

    generalized CV commits

--- frame.py
+++ frame.py
@@ -1,15 +1,17 @@
 from frame_mod import *
 from learn import *
 import numpy as np
+import re
 import argparse
 import csv
 
 parser = argparse.ArgumentParser(description='Framework that trains all learning options.')
 parser.add_argument('class1Directory' , action="store", help='Input Photo Directory, relative path')
 parser.add_argument('class2Directory' , action="store", help='Input Photo Directory, relative path')
-parser.add_argument('--hog' , action="store_true", default=False, help='Input Photo Directory, relative path')
-parser.add_argument('--bright' , action="store_true", default=False, help='Input Photo Directory, relative path')
-parser.add_argument('--random' , action="store_true", default=False, help='Input Photo Directory, relative path')
+parser.add_argument('number_k_folds', action="store", default=1, help='Input number of k-folds if you''d like to use k-fold cross validation')
+parser.add_argument('--hog' , action="store_true", default=False, help='Use hog features. If more than one feature selected, only first used.')
+parser.add_argument('--bright' , action="store_true", default=False, help='Use mean brightness feature. If more than one feature selected, only first used.')
+parser.add_argument('--random' , action="store_true", default=False, help='Use random subset of hog features. If more than one feature selected, only first used.')
 parser.add_argument('--all' , action="store_true", help='Runs all classifiers, skipping user input ')
 args = parser.parse_args()
 
@@ -22,49 +24,94 @@ def calculateError(predictions, testLabels, msg=''):
   print(("\t {} Error is " + str(error)).format(msg))
   return error
 
+# get disease name from file path using regexps
+str_p = re.compile('(str)',re.IGNORECASE)
+ptosis_p = re.compile('(ptosis)',re.IGNORECASE)
+osd_p = re.compile('(osd)',re.IGNORECASE)
+#pd4 = re.compile('(NEXT_NEW_DISEASE)',re.IGNORECASE)
+dirs = [args.class1Directory, args.class2Directory]
+diseases = ['hi','mom']
+for i in range(2):
+    dir2srch = dirs[i]
+    if str_p.search(dir2srch) and (not osd_p.search(dir2srch)) and (not ptosis_p.search(dir2srch)):
+        diseases[i] = 'str'
+    elif (not str_p.search(dir2srch)) and osd_p.search(dir2srch) and (not ptosis_p.search(dir2srch)):
+        diseases[i] = 'osd'
+    elif (not str_p.search(dir2srch)) and (not osd_p.search(dir2srch)) and ptosis_p.search(dir2srch):
+        diseases[i] = 'ptosis'
+    else:
+        print("Broski, your file paths are confusing. IDK what disease you're tryna classify. Make sure only 1 disease name is in the relative path.")
+        exit(1)
+
+if diseases[0]=='hi' or diseases[1]=='mom':
+    print('Broski, you messed up. I couldn''t identify both the diseases from your file paths')
+    exit(1)
+else:
+    dis_set = diseases[0] + '_' + diseases[1]
+
 def main():
+    # return indices of models being used
     if not args.all:
       selectedModels = selectModels(MODELS)
     else:
       selectedModels = range(len(MODELS))
 
+    if args.hog:
+        feat="hog"
+    elif args.bright:
+        feat="bright"
+    elif args.random:
+        feat="random"
+    else:
+        print("No feature type selected. Exiting...")
+        exit(1)
+
     # k-fold cross validation
-    folds = 5
-    num_models = 6
+    folds = int(args.number_k_folds)
+    # this is the total number of models possible. may be more than number
+    # currently being run
+    num_models = len(MODELS)
+    # the three layers of the error matrix are test error on class 1, test err cls.2 and train error
     error_matrix = np.zeros((3,folds,num_models))
     for k in range(1,folds+1):
       path = os.getcwd()
-      inputPath1 = os.path.join(path, args.class1Directory, "train_k"+str(k))
-      inputPath2 = os.path.join(path, args.class2Directory, "train_k"+str(k))
-      testPath1 = os.path.join(path, args.class1Directory, "test_k"+str(k))
-      testPath2 = os.path.join(path, args.class2Directory, "test_k"+str(k))
+      # if using k-fold cv, use specific directory names as formatted by partition
+      # for k-fold
+      if folds>1:
+          inputPath1 = os.path.join(path, args.class1Directory, "train_k"+str(k))
+          inputPath2 = os.path.join(path, args.class2Directory, "train_k"+str(k))
+          testPath1 = os.path.join(path, args.class1Directory, "test_k"+str(k))
+          testPath2 = os.path.join(path, args.class2Directory, "test_k"+str(k))
+      else:
+          inputPath1 = os.path.join(path, args.class1Directory)
+          inputPath2 = os.path.join(path, args.class2Directory)
+          testPath1 = os.path.join(path, args.class1Directory)
+          testPath2 = os.path.join(path, args.class2Directory)
 
       (rawTrainData1, trainLabels1) = importData(inputPath1, 1)
       (rawTrainData2, trainLabels2) = importData(inputPath2, 2)
       (rawTestData1, testLabels1) = importData(testPath1, 1)
       (rawTestData2, testLabels2) = importData(testPath2, 2)
 
+      trainLabels = trainLabels1 + trainLabels2
+
       printInputStats(inputPath1, inputPath2, testPath1,  testPath2, len(rawTrainData1), len(rawTrainData2), len(rawTestData1), len(rawTestData2))
 
-      if args.hog:
+      if feat=="hog":
         trainFeatures = getHogFeatures(rawTrainData1 + rawTrainData2, "train data")
         testFeatures1= getHogFeatures(rawTestData1, "test data")
         testFeatures2= getHogFeatures(rawTestData2, "test data")
-      elif args.bright:
+      elif feat=="bright":
         trainFeatures = np.array(getMeanBrightness(rawTrainData1 + rawTrainData2, "train data")).reshape(-1, 1)
         testFeatures1= np.array(getMeanBrightness(rawTestData1, "test data")).reshape(-1, 1)
         testFeatures2= np.array(getMeanBrightness(rawTestData2, "test data")).reshape(-1, 1)
-      elif args.random:
+      elif feat=="random":
         trainFeatures = getRandomFeatures(rawTrainData1 + rawTrainData2, "train data")
         testFeatures1= getRandomFeatures(rawTestData1, "test data")
         testFeatures2= getRandomFeatures(rawTestData2, "test data")
-      else:
-        print("No feature type selected. Exiting...")
-        exit(1)
 
-      trainLabels = trainLabels1 + trainLabels2
-      testLabels1 = trainLabels1
-      testLabels2 = trainLabels2
+#      testLabels1 = trainLabels1 # WHY was this still here??
+#      testLabels2 = trainLabels2
 
 
       for i in selectedModels:
@@ -83,11 +130,11 @@ def main():
     avg_err = np.sum(error_matrix,axis=1)/float(folds)
     #with open('error.csv','w') as csvfile:
     #    writer = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-    #    writer.writerow(['Diseases','Model','Features','Test1 Error','Test2 Error','Train Error'])
+    #    writer.writerow(['Diseases','Model','Features','Disease 1 Test Error','Disease 2 Test Error','Train Error'])
     for j in selectedModels:
         with open('error.csv','a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(['osd_ptosis',str(MODELS[j].__name__),'only hog rn',str(avg_err[0,j]),str(avg_err[1,j]),str(avg_err[2,j])])
+            writer.writerow([dis_set,str(MODELS[j].__name__),feat,str(avg_err[0,j]),str(avg_err[1,j]),str(avg_err[2,j])])
 
 if __name__ == '__main__':
   main()
